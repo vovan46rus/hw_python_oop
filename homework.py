b7b473c1644f385ba2b25b9538291a0ef27497b4
vovan46rus <punk.potap@gmail.com>
2023-02-11 13:18:16 +0300

    homework.py

--- homework.py
+++ homework.py
@@ -130,7 +130,8 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    sport_dict: Dict[str, Type[Training]] = {'SWM': Swimming,
+    sport_dict: Dict[str, Type[Training]] = {
+                                             'SWM': Swimming,
                                              'RUN': Running,
                                              'WLK': SportsWalking,
                                              }
